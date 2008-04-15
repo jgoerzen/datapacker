0d4e3ee6c4e5c780009ad5c09dfd79cc0df8ae90
John Goerzen <jgoerzen@complete.org>
2008-04-15 03:29:39 -0500

    Made binify_po smarter

--- Scan.hs
+++ Scan.hs
@@ -16,10 +16,10 @@ import System.Log.Logger
 scan :: RunInfo -> [FilePath] -> IO [[Result]]
 scan ri fplist =
     do sizes <- (liftM concat $ mapM getSize fplist)
-       if preserveOrder ri
-          then return $ binify_po bins sizes
---          else binify_opt bins sizes
-          else fail "Not done"
+       let func = if preserveOrder ri then binify_po else binify_po
+       case func bins sizes of
+         Left x -> fail x
+         Right x -> return x
     where getSize f = 
               do s <- getFileStatus f
                  if isRegularFile s
@@ -29,16 +29,18 @@ scan ri fplist =
                     
           bins = firstBinSize ri : repeat (binSize ri)
 
-binify_po :: (Num s, Ord s) => 
+binify_po :: (Num s, Ord s, Show o) => 
              [s]                -- ^ The sizes of bins.  List may be infinite.
-          -> [(s, a)]           -- ^ (size, object) pairs
-          -> [[(s, a)]]         -- ^ Results
-binify_po _ [] = []
+          -> [(s, o)]           -- ^ (size, object) pairs
+          -> Either String [[(s, o)]]         -- ^ Results or error
+binify_po _ [] = Right []                     -- ^ Ran out of sizes
+binify_po [] _ = Left "Ran out of bins"
 binify_po (thisbinsize:otherbins) sizes =
-    let thisset = fillBin 0 sizes
-        fillBin accumsize ((s, o):xs) 
-            | s > thisbinsize = error $ "Size " ++ show s ++ " greater than bin size " ++ show thisbinsize
-            | s + accumsize > thisbinsize = []
-            | otherwise = (s, o) : fillBin (accumsize + s) xs
-        in thisset : binify_po otherbins (drop (length thisset) sizes)
-
+    let fillBin accumsize ((s, o):xs) 
+            | s > thisbinsize = Left $ "Size " ++ show s ++ " greater than bin size " ++ show thisbinsize ++ " at " ++ show o
+            | s + accumsize > thisbinsize = Right []
+            | otherwise = do next <- fillBin (accumsize + s) xs
+                             return $ (s, o) : next
+        in do thisset <- fillBin 0 sizes
+              next <- binify_po otherbins (drop (length thisset) sizes)
+              return (thisset : next)
