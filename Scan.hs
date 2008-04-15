2c577906f59c051df1b28dcc0ad0feeaa09a073c
John Goerzen <jgoerzen@complete.org>
2008-04-15 03:51:04 -0500

    Added optimized packing

--- Scan.hs
+++ Scan.hs
@@ -12,11 +12,12 @@ import Types
 import Control.Monad(liftM)
 import System.Posix.Files
 import System.Log.Logger
+import Data.List
 
 scan :: RunInfo -> [FilePath] -> IO [[Result]]
 scan ri fplist =
     do sizes <- (liftM concat $ mapM getSize fplist)
-       let func = if preserveOrder ri then binify_po else binify_po
+       let func = if preserveOrder ri then binify_po else binify_opt
        case func bins sizes of
          Left x -> fail x
          Right x -> return x
@@ -44,4 +45,31 @@ binify_po (thisbinsize:otherbins) sizes =
                              return $ (s, o) : next
         in do thisset <- fillBin 0 sizes
               next <- binify_po otherbins (drop (length thisset) sizes)
+              return (thisset : next)
+
+binify_opt :: (Num s, Ord s, Show o, Ord o) => 
+              [s]                -- ^ The sizes of bins.  List may be infinite.
+           -> [(s, o)]           -- ^ (size, object) pairs
+           -> Either String [[(s, o)]]         -- ^ Results or error
+binify_opt bins sizes = binify_opt' bins (sort sizes)
+
+binify_opt' :: (Num s, Ord s, Show o) => 
+              [s]                -- ^ The sizes of bins.  List may be infinite.
+           -> [(s, o)]           -- ^ (size, object) pairs
+           -> Either String [[(s, o)]]         -- ^ Results or error
+binify_opt' _ [] = Right []                     -- ^ Ran out of sizes
+binify_opt' [] _ = Left "Ran out of bins"
+binify_opt' (thisbinsize:otherbins) sizes =
+    let fillBin _ [] = Right []
+        fillBin accumsize sizelist =
+            case break (\x -> (fst x) + accumsize < thisbinsize) sizelist of
+              (_, []) ->
+                  if accumsize == 0
+                     then Left $ "No items small enough to fit in bin " ++ show thisbinsize ++ "; remainder is " ++ show sizelist
+                     else Right []
+              (nonmatches, ((s, o):matchxs)) ->
+                  do next <- fillBin (accumsize + s) (nonmatches ++ matchxs)
+                     return $ (s, o) : next
+        in do thisset <- fillBin 0 sizes
+              next <- binify_opt' otherbins (drop (length thisset) sizes)
               return (thisset : next)
