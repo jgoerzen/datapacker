5f96cbc99afd6e3515270ebcaa486556410ce6ce
John Goerzen <jgoerzen@complete.org>
2008-04-15 03:07:47 -0500

    Compilation fixes

---
+++ Scan.hs
@@ -0,0 +1,39 @@
+{- 
+Copyright (C) 2006-2008 John Goerzen <jgoerzen@complete.org>
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+-}
+
+module Scan(scan) where
+import Types
+import Control.Monad(liftM)
+import System.Posix.Files
+import System.Log.Logger
+
+scan :: RunInfo -> [FilePath] -> IO [Result]
+scan ri fplist =
+    do sizes <- (liftM concat $ mapM getSize fplist)
+       if preserveOrder ri
+          then binify_po bins sizes
+--          else binify_opt bins sizes
+          else error "Not done"
+    where getSize f = 
+              do s <- getFileStatus f
+                 if isRegularFile s
+                    then return [(fileSize s, f)]
+                    else warningM "scan" $ "Warning: file " ++ f ++ " is not a regular file; skipping"
+                    
+          bins = firstBinSize ri : repeat (binSize ri)
+
+binify_po _ [] = []
+binify_po (thisbinsize:otherbins) sizes =
+    let thisset = fillBin 0 sizes
+        fillBin accumsize ((s, o):xs) 
+            | s > thisbinsize = error $ "Size " ++ show s ++ " greater than bin size " ++ show thisbinsize
+            | s + accumsize > thisbinsize = []
+            | otherwise = (s, o) : fillBin (accumsize + s) xs
+        in thisset : binify_po otherbins (drop (length thisset) sizes)
+
