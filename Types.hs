dc6e412d9e58027d421f42f129cd8b8af9ae2c8c
John Goerzen <jgoerzen@complete.org>
2008-04-15 01:45:34 -0500

    Checkpointing

---
+++ Types.hs
@@ -0,0 +1,19 @@
+{- 
+Copyright (C) 2006-2008 John Goerzen <jgoerzen@complete.org>
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+-}
+
+module Types where
+
+data RunInfo = 
+    RunInfo {binSize :: Integer,
+             firstBinSize :: Integer,
+             preserveOrder :: Bool}
+--             readNull :: Bool}
+    deriving (Eq, Ord, Read, Show)
+
+type Results = [(Int, FilePath)]
