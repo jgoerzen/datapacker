232b7948faa44aae24ccdd36307c620c91af24c8
John Goerzen <jgoerzen@complete.org>
2008-04-15 19:26:01 -0500

    Removed dh_make cruft

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the datapacker package
-#
-0 4	* * *	root	datapacker_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(datapacker):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="datapacker" command="/usr/bin/datapacker"
